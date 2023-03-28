c9253859b625e944264bace0d59514b57baa9b33
camposcostero22 <camposcostero22@gmail.com>
2023-03-28 10:49:40 -0400

    Archivos

---
+++ public/js/auth.js
@@ -0,0 +1,62 @@
+const url = window.location.hostname.includes('localhost')
+    ?'http://localhost:8080/api/auth/'
+    :'http://localhost:8080'
+
+function handleCredentialResponse(response) {
+    //Google token ID_TOKEN
+    const body = {id_token:response.credential}
+
+    console.log({response});
+    
+    fetch(url + 'google',{
+        method:'POST',
+        headers: { 
+            'Content-Type': 'application/json'
+         },
+        body: JSON.stringify( body )
+   })
+        .then(res => res.json())
+        .then(({token}) =>{
+           localStorage.setItem('token',token) 
+        })
+        .catch(console.warn);
+}
+
+
+const miFormulario = document.querySelector('form');
+
+miFormulario.addEventListener('submit', async(e)=>{
+    e.preventDefault();
+    const formData = {};
+
+    for(let el of miFormulario.elements){
+        if (el.name.length > 0) 
+            formData[el.name] = el.value
+    
+    }
+    await fetch(url ,{
+        method:'POST',
+        body:JSON.stringify(formData),
+        headers:{'Content-Type': 'application/json'}
+    })
+    .then(res => res.json())
+    .then(data =>{
+        console.log(data);
+    })
+    .catch(err =>{
+        console.log(err);
+    })
+});
+
+
+
+const button = document.getElementById('sing-out');
+button.onclick= ()=>{
+    console.log(google.accounts.id);
+    google.accounts.id.disableAutoSelect();
+
+    google.accounts.id.revoke(localStorage.getItem('email'),(done)=>{
+        localStorage.clear()
+        location.reload();
+    });
+}
---
+++ socket/controller.js
@@ -0,0 +1,10 @@
+const { Socket } = require("socket.io")
+
+
+const socketController =(socket = new Socket())=>{
+    
+}
+
+module.exports={
+    socketController
+}
